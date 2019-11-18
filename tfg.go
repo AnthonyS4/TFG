32ad5c6a7093ba68e913bf8ab05c91c8b1a94283
AnthonyS4 <anthonyquispe44@gmail.com>
2019-11-18 20:09:53 +0100

    Modify documentation in start function

--- tfg.go
+++ tfg.go
@@ -14,7 +14,7 @@ import (
 //
 func main() {
 	configuration := others.ObtainConfiguration() //Obtains a reference of a map with the configurations
-	//wireshark.ExecuteTshark(configuration)        //Executes tshark
+	wireshark.ExecuteTshark(configuration)        //Executes tshark
 	tsharkCommand := "blabal/ablaa7asdb/ > /home/anthony/TFG/archivos/paquetes.json"
 	filebeat.FilebeatExecution(configuration, tsharkCommand)
 	//elasticsearch.DeployElasticSearchNetwork(configuration) //Start the nodes execution
